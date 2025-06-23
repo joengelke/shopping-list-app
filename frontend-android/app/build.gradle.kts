a00402fc97dcd8ce277216715d4e8c1e27d6843f
Jonas Engelke <jonas-engelke@gmx.net>
2025-06-23 11:43:50 +0200

    new version

--- frontend-android/app/build.gradle.kts
+++ frontend-android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 35
         versionCode = 1
-        versionName = "0.2.1"
+        versionName = "0.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
